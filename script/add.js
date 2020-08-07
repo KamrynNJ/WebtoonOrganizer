993e080acba86c984da817a470b682a3d6cac3a0
KamrynNJ <51929727+KamrynNJ@users.noreply.github.com>
2020-08-07 14:28:07 -0400

    js file

---
+++ script/add.js
@@ -0,0 +1,9 @@
+function showAlert(){
+ alert("Hello World!");
+}
+function clickAddButton(){
+  var newDiv=document.createElement("div");
+  var newImg=document.createElement("img");
+  var newText=document.createElement("p");
+  newDiv.appendChild(newImg,newText);
+}
