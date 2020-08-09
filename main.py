4f8ffa0f93512143ab3bb9357f465898f9aa409b
KamrynNJ <51929727+KamrynNJ@users.noreply.github.com>
2020-08-09 18:16:17 -0400

    need to make objects save to database

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
 
 # This initializes the jinja2 Environment.
 # This will be the same in every app that uses the jinja2 templating library.
@@ -11,6 +12,11 @@ the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class NewWebtoon(ndb.Model):
+  title_class = ndb.StringProperty(required=True)
+  picture_class = ndb.StringProperty(required=True)
+  link_class= ndb.StringProperty(required=True)
 
 class HomePage(webapp2.RequestHandler):
     def get(self):  # for a get request
@@ -28,7 +34,7 @@ class ShowComic(webapp2.RequestHandler):
         the_variable_dict = {
             "title_from_form": title_of_comic,
             "pic_from_form": pic_of_comic,
-            "link_from_comic": link_of_comic
+            "link_from_comic": link_of_comic,
         }
         # pass that dictionary to the Jinja2 `.render()` method
         self.response.write(results_template.render(the_variable_dict))
@@ -36,5 +42,5 @@ class ShowComic(webapp2.RequestHandler):
 # the app configuration section
 app = webapp2.WSGIApplication([
     ('/', HomePage),
-    ('addComic.html', ShowComic), #this maps the root url to the Main Page Handler
+    ('/addComic.html', ShowComic), #this maps the root url to the Main Page Handler
 ], debug=True)
